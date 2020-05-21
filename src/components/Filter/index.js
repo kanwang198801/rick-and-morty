0d43954f0537cf323449af7ef5e9ce8619802f80
kan <kan.wang@arq.group>
2020-05-21 21:39:39 +1000

    added types and more species to filter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -27,9 +27,23 @@ export default function Search({ filterFunctions, filterValues }) {
                 <option value="">Select Species</option>
                 <option value="Human">Human</option>
                 <option value="Alien">Alien</option>
+                <option value="Humanoid">Humanoid</option>
+                <option value="Poopybutthole">Poopybutthole</option>
             </select>
             <select name="type" onChange={(e) => setTypeFilter(e.target.value)} value={typeFilter}>
                 <option value="">Select Type</option>
+                <option value="Superhuman (Ghost trains summoner)">Superhuman (Ghost trains summoner)</option>
+                <option value="Parasite, Cyborg">Parasite, Cyborg</option>
+                <option value="Human with antennae">Human with antennae</option>
+                <option value="Human with ants in his eyes">Human with ants in his eyes</option>
+                <option value="Fish-Person">Fish-Person</option>
+                <option value="Self-aware arm">Self-aware arm</option>
+                <option value="Cat-Person">Cat-Person</option>
+                <option value="Human with baby legs">Human with baby legs</option>
+                <option value="Parasite">Parasite</option>
+                <option value="Bepisian">Bepisian</option>
+                <option value="Hivemind">Hivemind</option>
+                <option value="Hivemind">Mytholog</option>
             </select>
             <select name="gender" onChange={(e) => setGenderFilter(e.target.value)} value={genderFilter}>
                 <option value="">Select Gender</option>
